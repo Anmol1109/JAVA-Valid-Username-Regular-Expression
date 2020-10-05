44606fda818049627accce2a0f2695b367e6a6ba
Anmol1109 <48082902+Anmol1109@users.noreply.github.com>
2020-10-05 22:20:44 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+import java.util.regex.*;
+class UsernameValidator 
+{
+    //String regex = "^[aA-zZ]\\w{7,29}$";
+    /*
+     * Write regular expression here.
+     */
+    public static final String regularExpression = "^[a-zA-Z][a-zA-Z0-9_]{7,29}$";
+}
+
+
+
+public class Solution {
+    private static final Scanner scan = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int n = Integer.parseInt(scan.nextLine());
+        while (n-- != 0) {
+            String userName = scan.nextLine();
+
+            if (userName.matches(UsernameValidator.regularExpression)) {
+                System.out.println("Valid");
+            } else {
+                System.out.println("Invalid");
+            }           
+        }
+    }
+}
